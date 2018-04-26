3fc45320e0a8640d02155c66b6192471b6095900
DaemonNikolay <nikolay-eckert@mail.ru>
2018-04-27 01:44:23 +0700

    Example read the file

---
+++ C#/ReadFile/ReadFile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ReadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fstream = new FileStream(@"C:\MyFiles\text.txt", FileMode.Open))
+            {
+                byte[] array = new byte[fstream.Length]; // считываем данные из файла в байты
+                fstream.Read(array, 0, array.Length);  // превращаем байты в строку
+                string textFromFile = Encoding.UTF8.GetString(array); // конвертируем строковое представление байтов в привычную строку
+
+                Console.WriteLine(textFromFile); // тестируем
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/ReadFile/ReadFile/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ReadFile")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReadFile")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("d13bd1dc-c8b3-4716-891b-b7152bf39510")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
